edbc757f9953d78a085ee07be27e771030f6d477
soumyadeep dey <105117395+soumyadeepdey2003@users.noreply.github.com>
2023-09-02 21:07:06 +0530

    Update main.dart

--- megatronix_app/lib/main.dart
+++ megatronix_app/lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:megatronix_app/Pages/About_Us.dart';
 import 'package:megatronix_app/Pages/Contact_Us.dart';
 import 'package:megatronix_app/Pages/Event.dart';
 import 'package:megatronix_app/Pages/Gallery.dart';
 import 'package:megatronix_app/Pages/Home.dart';
 import 'package:megatronix_app/Pages/Team.dart';
 
-/// Flutter code sample for [NavigationBar].
-
 void main() => runApp(const NavigationBarApp());
 
 class NavigationBarApp extends StatelessWidget {
-  const NavigationBarApp({super.key});
+  const NavigationBarApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +17,7 @@ class NavigationBarApp extends StatelessWidget {
 }
 
 class NavigationExample extends StatefulWidget {
-  const NavigationExample({super.key});
+  const NavigationExample({Key? key});
 
   @override
   State<NavigationExample> createState() => _NavigationExampleState();
@@ -32,110 +29,104 @@ class _NavigationExampleState extends State<NavigationExample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        backgroundColor: Color.fromARGB(255, 59, 58, 67),
-        onDestinationSelected: (int index) {
+      bottomNavigationBar: AnimatedNavigationBar(
+        currentIndex: currentPageIndex,
+        onTap: (int index) {
           setState(() {
             currentPageIndex = index;
           });
         },
-        indicatorColor: Color.fromARGB(255, 215, 12, 70),
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.home,
-              color: Colors.white,
-            ),
-            icon: Icon(
-              Icons.home_outlined,
-              color: Colors.white,
-            ),
-            label: 'Home',
-          ),
-          // NavigationDestination(
-          //   selectedIcon: Icon(Icons.search),
-          //   icon: Icon(Icons.search_outlined),
-          //   label: 'About Us',
-          // ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.event,
-              color: Colors.white,
-            ),
-            icon: Icon(
-              Icons.event_available_outlined,
-              color: Colors.white,
-            ),
-            label: 'Event',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.contact_page,
-              color: Colors.white,
-            ),
-            icon: Icon(
-              Icons.contact_page_outlined,
-              color: Colors.white,
-            ),
-            label: 'Contact Us',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.picture_in_picture_rounded,
-              color: Colors.white,
-            ),
-            icon: Icon(
-              Icons.picture_in_picture_alt_outlined,
-              color: Colors.white,
-            ),
-            label: 'Gallery',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.group,
-              color: Colors.white,
-            ),
-            icon: Icon(
-              Icons.group_add_outlined,
-              color: Colors.white,
-            ),
-            label: 'Team',
-          ),
-        ],
+        indicatorColor:
+            Color.fromARGB(255, 215, 12, 70), // Set the indicator color
       ),
-      body: <Widget>[
-        Container(
-          color: Colors.red,
-          alignment: Alignment.center,
-          child: HomePage(),
+      body: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        child: [
+          Container(
+            color: Colors.red,
+            alignment: Alignment.center,
+            key:
+                UniqueKey(), // UniqueKey triggers the AnimatedSwitcher animation
+            child: HomePage(),
+          ),
+          Container(
+            color: Colors.green,
+            alignment: Alignment.center,
+            key: UniqueKey(),
+            child: EventPage(),
+          ),
+          Container(
+            color: Colors.blue,
+            alignment: Alignment.center,
+            key: UniqueKey(),
+            child: ContactUsPage(),
+          ),
+          Container(
+            color: Colors.blue,
+            alignment: Alignment.center,
+            key: UniqueKey(),
+            child: GalleryPage(),
+          ),
+          Container(
+            color: Colors.blue,
+            alignment: Alignment.center,
+            key: UniqueKey(),
+            child: TeamPage(),
+          ),
+        ][currentPageIndex],
+      ),
+    );
+  }
+}
+
+class AnimatedNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+  final Color indicatorColor;
+
+  AnimatedNavigationBar({
+    required this.currentIndex,
+    required this.onTap,
+    required this.indicatorColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.black,
+      backgroundColor: Color.fromARGB(255, 219, 219, 225),
+      indicatorColor: indicatorColor, // Set the indicator color
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          activeIcon: Icon(Icons.home, color: Colors.white),
+          label: 'Home',
         ),
-        // Container(
-        //   color: Colors.red,
-        //   alignment: Alignment.center,
-        //   child: AboutUsPage(),
-        // ),
-        Container(
-          color: Colors.green,
-          alignment: Alignment.center,
-          child: EventPage(),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.event_available_outlined),
+          activeIcon: Icon(Icons.event, color: Colors.white),
+          label: 'Event',
         ),
-        Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: ContactUsPage(),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.contact_page_outlined),
+          activeIcon: Icon(Icons.contact_page, color: Colors.white),
+          label: 'Contact Us',
         ),
-        Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: GalleryPage(),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.picture_in_picture_alt_outlined),
+          activeIcon:
+              Icon(Icons.picture_in_picture_rounded, color: Colors.white),
+          label: 'Gallery',
         ),
-        Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: TeamPage(),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.group_add_outlined),
+          activeIcon: Icon(Icons.group, color: Colors.white),
+          label: 'Team',
         ),
-      ][currentPageIndex],
+      ],
     );
   }
 }
